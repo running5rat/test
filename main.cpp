8fb3816c8516e974b4d7da69789a5b9e33cdb545
doomhamster <42763011+running5rat@users.noreply.github.com>
2020-02-28 15:10:12 +0800

    第一次上传文件
    上传

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "qpushbutton.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QPushButton hello("HELLO WORLD!",0);
+    hello.resize(100,30);
+    //a.setMainWidget( &hello );
+    hello.show();
+    return a.exec();
+}
